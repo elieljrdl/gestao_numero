0c5e87790759344841617b410ba82ea8464e7cd7
eliel <elieljrdl1@gmail.com>
2026-03-03 16:46:48 -0300

    consulta numeros e whatsapp

--- src/services/NumeroServices.js
+++ src/services/NumeroServices.js
@@ -1,31 +1,78 @@
 const Services = require("./Services.js");
-const db = require('../database/models');
+const db = require("../database/models");
 
 class NumeroServices extends Services {
   constructor() {
     super("Numero");
   }
 
+  async getAllForDb(page, limit) {
+    const offset = (page - 1) * limit;
+    const resultado = await db[this.model].findAndCountAll({
+      limit,
+      offset,
+      include: [
+        {
+          model: db.Whatsapp,
+          as: "Whatsapp",
+          include: [
+            {
+              model: db.Perfil,
+              as: "Perfil",
+              include: [
+                {
+                  model: db.Celular,
+                  as: "Celular",
+                },
+              ],
+            },
+            {
+              model: db.Conexao,
+              as: "Conexao",
+              include: [
+                {
+                  model: db.Cliente,
+                  as: "Cliente",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+
+    return {
+      total: resultado.count,
+      paginaAtual: page,
+      totalPaginas: Math.ceil(resultado.count / limit),
+      dados: resultado.rows,
+    };
+  }
   // impedimos associação de um número a mais de um WhatsApp activo
   async createItem(bodyCreate) {
-
-    if (bodyCreate.whatsapp_id !== null && bodyCreate.whatsapp_id !== undefined) {
+    if (
+      bodyCreate.whatsapp_id !== null &&
+      bodyCreate.whatsapp_id !== undefined
+    ) {
       const exists = await db.Numero.findOne({
         where: { whatsapp_id: bodyCreate.whatsapp_id },
       });
-      
+
       if (exists) {
         const err = new Error("este whatsapp já está vinculado a outro numero");
         err.name = "BusinessRuleError";
         throw err;
       }
     }
-    
+
     return super.createItem(bodyCreate);
   }
 
   async updateItem(bodyUpdate, id) {
-    if (bodyUpdate.whatsapp_id !== null && bodyUpdate.whatsapp_id !== undefined) {
+    if (
+      bodyUpdate.whatsapp_id !== null &&
+      bodyUpdate.whatsapp_id !== undefined
+    ) {
       const conflict = await db.Numero.findOne({
         where: {
           whatsapp_id: bodyUpdate.whatsapp_id,
